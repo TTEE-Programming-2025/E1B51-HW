76337e8b05cb17229d18c4175d2b51d3fe3357f5
李信德 <billblabla1234@gmail.com>
2025-04-28 20:13:17 +0800

    1.更新個人風格頁面

---
+++ HW2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    int i,j,k,password;
+    char ch;
+    printf("     ___\n");//6到26行個人風格畫面 
+    printf("     \\ /\n");
+    for (i=0;i<=3;i++)
+    {
+    	printf("     |");
+    	printf("H");
+    	printf("|");
+    	printf("\n");
+	}
+    printf("/-----------\\\n");
+	printf("I<<<<<X>>>>>I\n");
+    printf("\\-----------/\n");
+    for (i=0;i<=12;i++)
+    {
+    	printf("     |");
+    	printf("H");
+    	printf("|");
+    	printf("\n");
+	}
+	printf("      V\n");
+	system("pause");
+	system("CLS");
+}
