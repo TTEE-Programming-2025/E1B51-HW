29ef594c5142edc17b75df338bb411ccb3442981
李信德 <billblabla1234@gmail.com>
2025-05-22 15:55:17 +0800

    增加個人風格頁面、密碼

--- HW4.c
+++ HW4.c
@@ -1,1 +1,45 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int i,j,k,l,m,password,wrong=0,num;
+char key,seat[9][9],sat;
+int main(void)
+{
+    printf("     ___\n");//personal style page
+    printf("     \\ /\n");
+    for (i=0;i<=3;i++)
+    {
+    	printf("     |");
+    	printf("H");
+    	printf("|");
+    	printf("\n");
+	}
+    printf("/-----------\\\n");
+	printf("I<<<<<X>>>>>I\n");
+    printf("\\-----------/\n");
+    for (i=0;i<=12;i++)
+    {
+    	printf("     |");
+    	printf("H");
+    	printf("|");
+    	printf("\n");
+	}
+	printf("      V\n");
+	system("pause");
+	system("CLS");
+	printf("請輸入密碼(2025):");
+	scanf("%d",&password);
+	while (password!=2025)//Determine the password
+	{
+		wrong++;
+		if (wrong==3)
+		{
+			printf("錯誤三次!");
+			return 0;
+		}
+		printf("請輸入密碼(2025):");
+		scanf("%d",&password);
+	}
+	printf("歡迎!\n");
+	system("pause");
+	system("CLS");
+}
