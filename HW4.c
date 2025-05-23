d81212c8fbc3c8f340a30603ddddf1ea6d661dff
李信德 <billblabla1234@gmail.com>
2025-05-23 21:30:18 +0800

    將abc部分優化為函數

--- HW4.c
+++ HW4.c
@@ -1,20 +1,129 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int i,j,k,password,wrong=1,num,ppl;
-char key,searchname[10];
-float avg[10];
-void checkscore(int *score)
-{
-	while (*score>100||*score<0)
-	{
-		printf("輸入錯誤請再次輸入:");
-		scanf("%d",score);
-	}
+
+#define MAX_STUDENTS 10
+int i,j,password,wrong=0;
+struct student {
+    char name[100];
+    char num[100];
+    int mscore;
+    int pscore;
+    int escore;
+    float avg;
+};
+// 檢查成績是否合法
+void checkScore(int *score) {
+    while (*score < 0 || *score > 100) {
+        printf("成績需介於 0 ~ 100，請重新輸入: ");
+        scanf("%d", score);
+    }
 }
-int main(void)
-{
-    printf("     ___\n");//personal style page
+
+// 計算所有學生的平均
+void calculateAverage(struct student data[], int ppl) {
+    for (i = 0; i < ppl; i++) {
+        data[i].avg = (data[i].mscore + data[i].pscore + data[i].escore) / 3.0;
+    }
+}
+
+// 輸入學生資料
+int enterGrades(struct student data[]) {
+    int ppl, i, j, k;
+    do {
+        printf("輸入 5~10 的學生人數: ");
+        scanf("%d", &ppl);
+        if (ppl < 5 || ppl > 10)
+            printf("輸入錯誤請再次輸入\n");
+    } while (ppl < 5 || ppl > 10);
+
+    for (i = 0; i < ppl; i++) {
+        printf("輸入第 %d 位學生的姓名: ", i + 1);
+        scanf("%s", data[i].name);
+
+        do {
+            printf("輸入第 %d 位學生的學號(6位數): ", i + 1);
+            scanf("%s", data[i].num);
+            for (j = 0; data[i].num[j] != '\0'; j++);
+            if (j != 6) {
+                printf("學號需為6位，請重新輸入。\n");
+                continue;
+            }
+
+            int duplicate = 0;
+            for (k = 0; k < i; k++) {
+                if (strcmp(data[i].num, data[k].num) == 0) {
+                    printf("學號重複，請重新輸入。\n");
+                    duplicate = 1;
+                    break;
+                }
+            }
+            if (!duplicate && j == 6)
+                break;
+        } while (1);
+
+        printf("輸入數學成績: ");
+        scanf("%d", &data[i].mscore);
+        checkScore(&data[i].mscore);
+
+        printf("輸入物理成績: ");
+        scanf("%d", &data[i].pscore);
+        checkScore(&data[i].pscore);
+
+        printf("輸入英文成績: ");
+        scanf("%d", &data[i].escore);
+        checkScore(&data[i].escore);
+    }
+
+    calculateAverage(data, ppl);
+    return ppl;
+}
+// 顯示成績
+void displayGrades(struct student data[], int ppl) {
+    printf("姓名  學號     數學  物理  英文  平均\n");
+    for (i = 0; i < ppl; i++) {
+        printf("%s  %s  %d  %d  %d  %.1f\n",
+               data[i].name, data[i].num,
+               data[i].mscore, data[i].pscore,
+               data[i].escore, data[i].avg);
+    }
+}
+
+// 搜尋學生
+void searchStudent(struct student data[], int ppl) {
+    char searchname[100];
+    int found = 0;
+    printf("輸入搜尋學生姓名: ");
+    scanf("%s", searchname);
+
+    for (i = 0; i < ppl; i++) {
+        if (strcmp(searchname, data[i].name) == 0) {
+            printf("姓名  學號     數學  物理  英文  平均\n");
+            printf("%s  %s  %d  %d  %d  %.1f\n",
+                   data[i].name, data[i].num,
+                   data[i].mscore, data[i].pscore,
+                   data[i].escore, data[i].avg);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("找不到該學生。\n");
+    }
+    system("pause");
+    system("CLS");
+}
+
+
+void checkScore(int *score);
+void calculateAverage(struct student data[], int ppl);
+int enterGrades(struct student data[]);
+void displayGrades(struct student data[], int ppl);
+void searchStudent(struct student data[], int ppl);
+
+int main() {
+	printf("     ___\n");//personal style page
     printf("     \\ /\n");
     for (i=0;i<=3;i++)
     {
@@ -52,110 +161,36 @@ int main(void)
 	printf("歡迎!\n");
 	system("pause");
 	system("CLS");
-	struct student
-	{
-		char name[100];
-		char num[100];
-		int mscore;
-		int pscore;
-		int escore;
-	};
-	struct student data[10];
-	while (1)
-	{
-		printf("------------[Grade System]----------\n");
-		printf("| a. Enter student grades          |\n");
-		printf("| b. Display student grades        |\n");
-		printf("| c. Search for student grades     |\n");
-		printf("| d. Grade ranking                 |\n");
-		printf("| e. Exit system                   |\n");
-		printf("------------------------------------\n");
-		printf("請輸入:");
-		scanf(" %c",&key);
-		if (key=='a')
-		{
-			system("CLS");
-			do
-			{
-				printf("輸入5~10的整數:");
-				scanf("%d",&ppl);
-				system("CLS");
-				if (ppl>10||ppl<5)
-				{
-					printf("輸入錯誤請再次輸入\n");
-				}
-			}
-			while (ppl>10||ppl<5);
-			for (i=0;i<ppl;i++)
-			{
-				printf("輸入第%d個學生的姓名:",i+1);
-				scanf("%s",data[i].name);
-				printf("輸入第%d個學生的學號(6位整數):",i+1);
-				scanf("%s",data[i].num);
-				for (k=0;k<i;k++)
-				{
-					while (strcmp(data[i].num,data[k].num)==0)
-					{
-						printf("重複學號，請再次輸入:");
-						scanf("%s",&data[i].num);
-					}
-				}
-				for (j=0;data[i].num[j]!='\0';j++);
-				while (j!=6)
-				{
-					printf("輸入錯誤請再次輸入:");
-					scanf("%s",&data[i].num);
-					for (j=0;data[i].num[j]!='\0';j++);
-				}
-				printf("輸入第%d個學生的數學成績:",i+1);
-				scanf("%d",&data[i].mscore);
-				checkscore (&data[i].mscore);
-				printf("輸入第%d個學生的物理成績:",i+1);
-				scanf("%d",&data[i].pscore);
-				checkscore (&data[i].pscore);
-				printf("輸入第%d個學生的英文成績:",i+1);
-				scanf("%d",&data[i].escore);
-				checkscore (&data[i].escore);
-				system("CLS");
-			}
-		}
-		if (key=='b')
-		{
-			system("CLS");
-			printf ("姓名  學號     數學  物理  英文  平均\n");
-			for (i=0;i<ppl;i++)
-			{
-				avg[i]=(data[i].mscore+data[i].pscore+data[i].escore)/3.0;
-				printf("%s  %s  %d  %d  %d  %.1f\n",data[i].name,data[i].num,data[i].mscore,data[i].pscore,data[i].escore,avg[i]);
-			}
-			system("pause");
-			system("CLS");
-		} 
-		if (key=='c')
-		{
-			system("CLS");
-			printf("輸入搜尋學生姓名:");
-			scanf("%s",searchname);
-			for (i=0;i<ppl;i++)
-			{
-				if (strcmp(searchname,data[i].name)==0)
-				{
-					wrong=0;
-					printf ("姓名  學號     數學  物理  英文  平均\n");
-					printf("%s  %s  %d  %d  %d  %.1f\n",data[i].name,data[i].num,data[i].mscore,data[i].pscore,data[i].escore,avg[i]);
-					system("pause");
-					system("CLS");
-				}
-			}
-			if (wrong)
-			{
-				printf("該學生不存在\n");
-				system("pause");
-				system("CLS");
-			}
-		}
-	}
-	
+    struct student data[MAX_STUDENTS];
+    char key;
+    int ppl = 0;
 
-	
+    while (1) {
+        printf("------------[Grade System]----------\n");
+        printf("| a. Enter student grades          |\n");
+        printf("| b. Display student grades        |\n");
+        printf("| c. Search for student grades     |\n");
+        printf("| d. Grade ranking                 |\n");
+        printf("| e. Exit system                   |\n");
+        printf("------------------------------------\n");
+        printf("請輸入: ");
+        scanf(" %c", &key);
+
+        if (key == 'a') {
+            system("CLS");
+            ppl = enterGrades(data);
+            system("CLS");
+        } else if (key == 'b') {
+            system("CLS");
+            displayGrades(data, ppl);
+            system("pause");
+            system("CLS");
+        } else if (key == 'c') {
+            system("CLS");
+            searchStudent(data, ppl);
+        } else if (key == 'e') {
+            break;
+        }
+    }
+    return 0;
 }
